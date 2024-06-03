6627c850595c0196a2ac0413668fbbbcd156712e
@MarlonLedezma <ledezmasolis@outlook.es>
2024-06-03 11:51:59 -0600

    #

--- Precentacion/Login/Login/frmLogin.Designer.cs
+++ Precentacion/Login/Login/frmLogin.Designer.cs
@@ -49,7 +49,7 @@
             // 
             // btnAccess
             // 
-            this.btnAccess.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnAccess.BackColor = System.Drawing.Color.SeaGreen;
             this.btnAccess.ForeColor = System.Drawing.Color.White;
             this.btnAccess.Location = new System.Drawing.Point(452, 199);
             this.btnAccess.Name = "btnAccess";
@@ -171,7 +171,7 @@
             this.AcceptButton = this.btnAccess;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackColor = System.Drawing.Color.DarkOrange;
             this.ClientSize = new System.Drawing.Size(698, 317);
             this.Controls.Add(this.btnViewPassWord);
             this.Controls.Add(this.lblForgetPassword);
