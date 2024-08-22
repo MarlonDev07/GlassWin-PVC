da9320fcf88bec77f26a6d2c0276016b071e9c83
alexDurVar <alexduva21@gmail.com>
2024-08-22 10:55:26 -0600

    Se arregló lo del proveedor en el desglose

--- Precentacion/User/Bill/frmFacturar.cs
+++ Precentacion/User/Bill/frmFacturar.cs
@@ -237,7 +237,7 @@ namespace Precentacion.User.Bill
                             Material = "1 3/4x4";
                         }
                         //Obtener el Total del Aluminio del Vidrio Fijo                      
-                        dtAluminio = NLoadProduct.loadAluminioVentanaFijaDesglose(Color, Sistema, "Extralum", Material);
+                        dtAluminio = NLoadProduct.loadAluminioVentanaFijaDesglose(Color, Sistema, cbProveedorDesglose.Text, Material);
 
                         //Obtener el Metraje del dt Aluminio y Multiplicarlo por la Cantidad de Ventanas
                         foreach (DataRow item in dtAluminio.Rows)
@@ -253,7 +253,7 @@ namespace Precentacion.User.Bill
                             string Descripcion = row.Cells[1].Value.ToString();
 
                             // Obtener el Total del Aluminio del Vidrio Fijo                      
-                            DataTable dtAluminioFijo = NLoadProduct.LoadAluminioFijoDesglose(Color, "Vidrio Fijo", "Extralum", anchoFijo, altoFijo, material, divisiones);
+                            DataTable dtAluminioFijo = NLoadProduct.LoadAluminioFijoDesglose(Color, "Vidrio Fijo", cbProveedorDesglose.Text, anchoFijo, altoFijo, material, divisiones);
 
                             // Obtener el Metraje del dtAluminioFijo y multiplicarlo por la Cantidad de Ventanas
                             foreach (DataRow item in dtAluminioFijo.Rows)
@@ -266,7 +266,7 @@ namespace Precentacion.User.Bill
                         }
 
                         // Obtener el Total del Aluminio                     
-                        DataTable dtAluminioSistema = NLoadProduct.loadAluminioDesglose(Color, Sistema, "Extralum");
+                        DataTable dtAluminioSistema = NLoadProduct.loadAluminioDesglose(Color, Sistema, cbProveedorDesglose.Text);
 
                         // Obtener el Metraje del dtAluminioSistema y multiplicarlo por la Cantidad de Ventanas                    
                         foreach (DataRow item in dtAluminioSistema.Rows)
@@ -316,7 +316,7 @@ namespace Precentacion.User.Bill
                     DataTable dtAccesorios = new DataTable();
 
                     //Obtener el Total de los Accesorios            
-                    dtAccesorios = NLoadProduct.loadAccesoriosDesglose(Sistema, "Extralum");
+                    dtAccesorios = NLoadProduct.loadAccesoriosDesglose(Sistema, cbProveedorDesglose.Text);
 
                     //Obtener el Metraje del dt Accesorios y Multiplicarlo por la Cantidad de Ventanas
                     foreach (DataRow item in dtAccesorios.Rows)
@@ -358,7 +358,7 @@ namespace Precentacion.User.Bill
                         string Descripcion = row.Cells[1].Value.ToString();
 
                         // Obtener el Total del Vidrio Fijo
-                        DataTable dtVidriosFijo = NLoadProduct.LoadPriceNewGlassDesglose("Extralum", vidrioFijo, anchoFijo, altoFijo);
+                        DataTable dtVidriosFijo = NLoadProduct.LoadPriceNewGlassDesglose(cbProveedorDesglose.Text, vidrioFijo, anchoFijo, altoFijo);
 
                         // Obtener el Metraje del dtVidriosFijo y multiplicarlo por la Cantidad de Ventanas
                         foreach (DataRow item in dtVidriosFijo.Rows)
@@ -371,7 +371,7 @@ namespace Precentacion.User.Bill
                     }
 
                     // Obtener el Total de los Vidrios (del sistema general)
-                    DataTable dtVidriosSistema = NLoadProduct.loadPricesGlassDesglose("Extralum", Vidrio);
+                    DataTable dtVidriosSistema = NLoadProduct.loadPricesGlassDesglose(cbProveedorDesglose.Text, Vidrio);
 
                     // Obtener el Metraje del dtVidriosSistema y multiplicarlo por la Cantidad de Ventanas
                     foreach (DataRow item in dtVidriosSistema.Rows)
---
+++ Precentacion/User/Quote/Quote/frmDesglose.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Precentacion.User.Quote.Quote
+{
+    partial class frmDesglose
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvDesglose = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDesglose)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvDesglose
+            // 
+            this.dgvDesglose.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvDesglose.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDesglose.Location = new System.Drawing.Point(12, 12);
+            this.dgvDesglose.Name = "dgvDesglose";
+            this.dgvDesglose.Size = new System.Drawing.Size(729, 402);
+            this.dgvDesglose.TabIndex = 1;
+            // 
+            // frmDesglose
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvDesglose);
+            this.Name = "frmDesglose";
+            this.Text = "frmDesglose";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDesglose)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvDesglose;
+    }
+}
