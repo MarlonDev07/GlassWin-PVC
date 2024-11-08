d6c11f48732ad687f98d2ad9da9ed99277a6247e
Ledez <Ledez@DESKTOP-I18D50B>
2024-11-08 07:45:31 -0600

    Actualización de conexiones y ajustes en formularios
    Se ha actualizado la cadena de conexión de la base de datos `GlassWinDB` en el archivo `App.config` para apuntar a la base de datos `GlassWin_PVC` en lugar de `glasswinPruebas`.
    
    Se han realizado ajustes menores en las posiciones de varios controles en el formulario `frmCalcPriceWindows.Designer.cs`.
    
    En el formulario `frmSelectSystem.Designer.cs` se han eliminado varios botones y paneles relacionados con los sistemas Europa y puertas. También se ha ajustado la posición del botón `btnBackSistema`, se ha reducido el tamaño del panel `panel6` y del formulario `frmSelectSystem`.
    
    Se han realizado cambios en el archivo `frmSelectSystem.resx` que consisten en la adición y eliminación de líneas de código.

--- Precentacion/User/Quote/Windows/Calculos de Precio/frmCalcPriceWindows.Designer.cs
+++ Precentacion/User/Quote/Windows/Calculos de Precio/frmCalcPriceWindows.Designer.cs
@@ -184,7 +184,7 @@
             this.txtUbicacion.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.txtUbicacion.BackColor = System.Drawing.Color.Orange;
             this.txtUbicacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtUbicacion.Location = new System.Drawing.Point(644, 8);
+            this.txtUbicacion.Location = new System.Drawing.Point(639, 8);
             this.txtUbicacion.MaxLength = 30;
             this.txtUbicacion.Name = "txtUbicacion";
             this.txtUbicacion.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
@@ -1040,7 +1040,7 @@
             this.lblDescripcion.ForeColor = System.Drawing.Color.Black;
             this.lblDescripcion.Location = new System.Drawing.Point(272, 7);
             this.lblDescripcion.Name = "lblDescripcion";
-            this.lblDescripcion.Size = new System.Drawing.Size(278, 24);
+            this.lblDescripcion.Size = new System.Drawing.Size(273, 24);
             this.lblDescripcion.TabIndex = 31;
             this.lblDescripcion.Text = "Descripción";
             // 
@@ -1050,7 +1050,7 @@
             this.label1.BackColor = System.Drawing.Color.Orange;
             this.label1.Font = new System.Drawing.Font("Impact", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(556, 6);
+            this.label1.Location = new System.Drawing.Point(551, 6);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(82, 24);
             this.label1.TabIndex = 63;
@@ -1475,7 +1475,7 @@
             // 
             this.btnCerrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btnCerrar.Image = ((System.Drawing.Image)(resources.GetObject("btnCerrar.Image")));
-            this.btnCerrar.Location = new System.Drawing.Point(1074, 3);
+            this.btnCerrar.Location = new System.Drawing.Point(1069, 3);
             this.btnCerrar.Name = "btnCerrar";
             this.btnCerrar.Size = new System.Drawing.Size(36, 29);
             this.btnCerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -1487,7 +1487,7 @@
             // 
             this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(1002, 3);
+            this.pictureBox1.Location = new System.Drawing.Point(997, 3);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(51, 29);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
--- Precentacion/User/Quote/Windows/Seleccion Sistema/frmSelectSystem.Designer.cs
+++ Precentacion/User/Quote/Windows/Seleccion Sistema/frmSelectSystem.Designer.cs
@@ -37,15 +37,8 @@
             this.btn8040_3_Vias = new System.Windows.Forms.Button();
             this.btn6030_2_Vias = new System.Windows.Forms.Button();
             this.btn6030_3_Vias = new System.Windows.Forms.Button();
-            this.btnEuro_2_Vias = new System.Windows.Forms.Button();
-            this.btnEuro_3_Vias = new System.Windows.Forms.Button();
             this.btnBackSistema = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.btnEUFijo = new System.Windows.Forms.Button();
-            this.btnEuPuerta2Vias = new System.Windows.Forms.Button();
-            this.btnEuPuerta3Vias = new System.Windows.Forms.Button();
-            this.btnPuertaLujo = new System.Windows.Forms.Button();
-            this.btnPuertaBaño = new System.Windows.Forms.Button();
             this.btnCedazoAkari = new System.Windows.Forms.Button();
             this.lblVtNormal = new System.Windows.Forms.Label();
             this.PanelSistemaTradicional = new System.Windows.Forms.Panel();
@@ -57,23 +50,11 @@
             this.PanlAkari = new System.Windows.Forms.Panel();
             this.panel1 = new System.Windows.Forms.Panel();
             this.lblSistemasAkari = new System.Windows.Forms.Label();
-            this.lblSistemasEuropa = new System.Windows.Forms.Label();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel5 = new System.Windows.Forms.Panel();
-            this.btnPuertaEuAbatible = new System.Windows.Forms.Button();
-            this.btnPuertaLiviana = new System.Windows.Forms.Button();
-            this.lblPuertas = new System.Windows.Forms.Label();
             this.panel6 = new System.Windows.Forms.Panel();
             this.PanelSistemaTradicional.SuspendLayout();
             this.PanelContenedorSistemaTradicional.SuspendLayout();
             this.PanlAkari.SuspendLayout();
             this.panel1.SuspendLayout();
-            this.panel3.SuspendLayout();
-            this.panel4.SuspendLayout();
-            this.panel2.SuspendLayout();
-            this.panel5.SuspendLayout();
             this.panel6.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -173,30 +154,6 @@
             this.btn6030_3_Vias.UseVisualStyleBackColor = true;
             this.btn6030_3_Vias.Click += new System.EventHandler(this.btn6030_3_Vias_Click);
             // 
-            // btnEuro_2_Vias
-            // 
-            this.btnEuro_2_Vias.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnEuro_2_Vias.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnEuro_2_Vias.BackgroundImage")));
-            this.btnEuro_2_Vias.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnEuro_2_Vias.Location = new System.Drawing.Point(0, 25);
-            this.btnEuro_2_Vias.Name = "btnEuro_2_Vias";
-            this.btnEuro_2_Vias.Size = new System.Drawing.Size(180, 71);
-            this.btnEuro_2_Vias.TabIndex = 9;
-            this.btnEuro_2_Vias.UseVisualStyleBackColor = true;
-            this.btnEuro_2_Vias.Click += new System.EventHandler(this.btnEuro_2_Vias_Click);
-            // 
-            // btnEuro_3_Vias
-            // 
-            this.btnEuro_3_Vias.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnEuro_3_Vias.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnEuro_3_Vias.BackgroundImage")));
-            this.btnEuro_3_Vias.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnEuro_3_Vias.Location = new System.Drawing.Point(220, 25);
-            this.btnEuro_3_Vias.Name = "btnEuro_3_Vias";
-            this.btnEuro_3_Vias.Size = new System.Drawing.Size(180, 71);
-            this.btnEuro_3_Vias.TabIndex = 10;
-            this.btnEuro_3_Vias.UseVisualStyleBackColor = true;
-            this.btnEuro_3_Vias.Click += new System.EventHandler(this.btnEuro_3_Vias_Click);
-            // 
             // btnBackSistema
             // 
             this.btnBackSistema.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
@@ -205,7 +162,7 @@
             this.btnBackSistema.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btnBackSistema.FlatAppearance.BorderSize = 0;
             this.btnBackSistema.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnBackSistema.Location = new System.Drawing.Point(806, 391);
+            this.btnBackSistema.Location = new System.Drawing.Point(806, 211);
             this.btnBackSistema.Name = "btnBackSistema";
             this.btnBackSistema.Size = new System.Drawing.Size(57, 47);
             this.btnBackSistema.TabIndex = 29;
@@ -224,68 +181,6 @@
             this.button1.TabIndex = 30;
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.btnVidrioFijo_Click);
-            // 
-            // btnEUFijo
-            // 
-            this.btnEUFijo.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnEUFijo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnEUFijo.BackgroundImage")));
-            this.btnEUFijo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnEUFijo.Location = new System.Drawing.Point(877, 25);
-            this.btnEUFijo.Name = "btnEUFijo";
-            this.btnEUFijo.Size = new System.Drawing.Size(180, 71);
-            this.btnEUFijo.TabIndex = 31;
-            this.btnEUFijo.UseVisualStyleBackColor = true;
-            this.btnEUFijo.Click += new System.EventHandler(this.btnEUFijo_Click);
-            // 
-            // btnEuPuerta2Vias
-            // 
-            this.btnEuPuerta2Vias.BackColor = System.Drawing.Color.Transparent;
-            this.btnEuPuerta2Vias.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnEuPuerta2Vias.BackgroundImage")));
-            this.btnEuPuerta2Vias.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnEuPuerta2Vias.Location = new System.Drawing.Point(447, 26);
-            this.btnEuPuerta2Vias.Name = "btnEuPuerta2Vias";
-            this.btnEuPuerta2Vias.Size = new System.Drawing.Size(180, 71);
-            this.btnEuPuerta2Vias.TabIndex = 32;
-            this.btnEuPuerta2Vias.UseVisualStyleBackColor = false;
-            this.btnEuPuerta2Vias.Click += new System.EventHandler(this.btnEuPuerta2Vias_Click);
-            // 
-            // btnEuPuerta3Vias
-            // 
-            this.btnEuPuerta3Vias.BackColor = System.Drawing.Color.Transparent;
-            this.btnEuPuerta3Vias.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnEuPuerta3Vias.BackgroundImage")));
-            this.btnEuPuerta3Vias.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnEuPuerta3Vias.Location = new System.Drawing.Point(669, 26);
-            this.btnEuPuerta3Vias.Name = "btnEuPuerta3Vias";
-            this.btnEuPuerta3Vias.Size = new System.Drawing.Size(180, 71);
-            this.btnEuPuerta3Vias.TabIndex = 33;
-            this.btnEuPuerta3Vias.UseVisualStyleBackColor = false;
-            this.btnEuPuerta3Vias.Click += new System.EventHandler(this.btnEuPuerta3Vias_Click);
-            // 
-            // btnPuertaLujo
-            // 
-            this.btnPuertaLujo.BackColor = System.Drawing.Color.Transparent;
-            this.btnPuertaLujo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnPuertaLujo.BackgroundImage")));
-            this.btnPuertaLujo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnPuertaLujo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnPuertaLujo.Location = new System.Drawing.Point(6, 12);
-            this.btnPuertaLujo.Name = "btnPuertaLujo";
-            this.btnPuertaLujo.Size = new System.Drawing.Size(180, 71);
-            this.btnPuertaLujo.TabIndex = 34;
-            this.btnPuertaLujo.UseVisualStyleBackColor = false;
-            this.btnPuertaLujo.Click += new System.EventHandler(this.btnPuertaLujo_Click);
-            // 
-            // btnPuertaBaño
-            // 
-            this.btnPuertaBaño.BackColor = System.Drawing.Color.Transparent;
-            this.btnPuertaBaño.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnPuertaBaño.BackgroundImage")));
-            this.btnPuertaBaño.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnPuertaBaño.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnPuertaBaño.Location = new System.Drawing.Point(224, 12);
-            this.btnPuertaBaño.Name = "btnPuertaBaño";
-            this.btnPuertaBaño.Size = new System.Drawing.Size(180, 71);
-            this.btnPuertaBaño.TabIndex = 35;
-            this.btnPuertaBaño.UseVisualStyleBackColor = false;
-            this.btnPuertaBaño.Click += new System.EventHandler(this.btnPuertaBaño_Click);
             // 
             // btnCedazoAkari
             // 
@@ -428,105 +323,6 @@
             this.lblSistemasAkari.Text = "Sistemas Akari";
             this.lblSistemasAkari.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // lblSistemasEuropa
-            // 
-            this.lblSistemasEuropa.BackColor = System.Drawing.Color.Black;
-            this.lblSistemasEuropa.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.lblSistemasEuropa.Font = new System.Drawing.Font("Impact", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSistemasEuropa.ForeColor = System.Drawing.Color.White;
-            this.lblSistemasEuropa.Location = new System.Drawing.Point(8, 197);
-            this.lblSistemasEuropa.Name = "lblSistemasEuropa";
-            this.lblSistemasEuropa.Size = new System.Drawing.Size(425, 49);
-            this.lblSistemasEuropa.TabIndex = 42;
-            this.lblSistemasEuropa.Text = "Sistemas Europa";
-            this.lblSistemasEuropa.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // panel3
-            // 
-            this.panel3.AutoScroll = true;
-            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel3.Controls.Add(this.panel4);
-            this.panel3.Location = new System.Drawing.Point(8, 250);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(426, 136);
-            this.panel3.TabIndex = 40;
-            // 
-            // panel4
-            // 
-            this.panel4.BackColor = System.Drawing.Color.White;
-            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.panel4.Controls.Add(this.btnEuro_2_Vias);
-            this.panel4.Controls.Add(this.btnEuro_3_Vias);
-            this.panel4.Controls.Add(this.btnEuPuerta2Vias);
-            this.panel4.Controls.Add(this.btnEuPuerta3Vias);
-            this.panel4.Controls.Add(this.btnEUFijo);
-            this.panel4.Location = new System.Drawing.Point(-1, -1);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(1083, 118);
-            this.panel4.TabIndex = 0;
-            // 
-            // panel2
-            // 
-            this.panel2.AutoScroll = true;
-            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel2.Controls.Add(this.panel5);
-            this.panel2.Location = new System.Drawing.Point(439, 250);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(423, 136);
-            this.panel2.TabIndex = 43;
-            // 
-            // panel5
-            // 
-            this.panel5.BackColor = System.Drawing.Color.White;
-            this.panel5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.panel5.Controls.Add(this.btnPuertaEuAbatible);
-            this.panel5.Controls.Add(this.btnPuertaLiviana);
-            this.panel5.Controls.Add(this.btnPuertaLujo);
-            this.panel5.Controls.Add(this.btnPuertaBaño);
-            this.panel5.Location = new System.Drawing.Point(-1, 13);
-            this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(884, 104);
-            this.panel5.TabIndex = 0;
-            // 
-            // btnPuertaEuAbatible
-            // 
-            this.btnPuertaEuAbatible.BackColor = System.Drawing.Color.Transparent;
-            this.btnPuertaEuAbatible.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnPuertaEuAbatible.BackgroundImage")));
-            this.btnPuertaEuAbatible.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnPuertaEuAbatible.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnPuertaEuAbatible.Location = new System.Drawing.Point(691, 12);
-            this.btnPuertaEuAbatible.Name = "btnPuertaEuAbatible";
-            this.btnPuertaEuAbatible.Size = new System.Drawing.Size(180, 71);
-            this.btnPuertaEuAbatible.TabIndex = 37;
-            this.btnPuertaEuAbatible.UseVisualStyleBackColor = false;
-            this.btnPuertaEuAbatible.Click += new System.EventHandler(this.btnPuertaEuAbatible_Click);
-            // 
-            // btnPuertaLiviana
-            // 
-            this.btnPuertaLiviana.BackColor = System.Drawing.Color.Transparent;
-            this.btnPuertaLiviana.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnPuertaLiviana.BackgroundImage")));
-            this.btnPuertaLiviana.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnPuertaLiviana.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnPuertaLiviana.Location = new System.Drawing.Point(465, 12);
-            this.btnPuertaLiviana.Name = "btnPuertaLiviana";
-            this.btnPuertaLiviana.Size = new System.Drawing.Size(180, 71);
-            this.btnPuertaLiviana.TabIndex = 36;
-            this.btnPuertaLiviana.UseVisualStyleBackColor = false;
-            this.btnPuertaLiviana.Click += new System.EventHandler(this.btnPuertaLiviana_Click);
-            // 
-            // lblPuertas
-            // 
-            this.lblPuertas.BackColor = System.Drawing.Color.Black;
-            this.lblPuertas.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.lblPuertas.Font = new System.Drawing.Font("Impact", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPuertas.ForeColor = System.Drawing.Color.White;
-            this.lblPuertas.Location = new System.Drawing.Point(438, 198);
-            this.lblPuertas.Name = "lblPuertas";
-            this.lblPuertas.Size = new System.Drawing.Size(423, 49);
-            this.lblPuertas.TabIndex = 44;
-            this.lblPuertas.Text = "Puertas";
-            this.lblPuertas.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
             // panel6
             // 
             this.panel6.BackColor = System.Drawing.Color.White;
@@ -534,15 +330,11 @@
             this.panel6.Controls.Add(this.lblSistemasAkari);
             this.panel6.Controls.Add(this.PanlAkari);
             this.panel6.Controls.Add(this.lblVtNormal);
-            this.panel6.Controls.Add(this.panel3);
-            this.panel6.Controls.Add(this.lblPuertas);
             this.panel6.Controls.Add(this.PanelSistemaTradicional);
-            this.panel6.Controls.Add(this.lblSistemasEuropa);
-            this.panel6.Controls.Add(this.panel2);
             this.panel6.Location = new System.Drawing.Point(26, 66);
             this.panel6.Margin = new System.Windows.Forms.Padding(2);
             this.panel6.Name = "panel6";
-            this.panel6.Size = new System.Drawing.Size(868, 441);
+            this.panel6.Size = new System.Drawing.Size(868, 261);
             this.panel6.TabIndex = 45;
             this.panel6.Paint += new System.Windows.Forms.PaintEventHandler(this.panel6_Paint);
             // 
@@ -553,7 +345,7 @@
             this.AutoScroll = true;
             this.BackColor = System.Drawing.Color.LightGray;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(923, 547);
+            this.ClientSize = new System.Drawing.Size(923, 343);
             this.Controls.Add(this.panel6);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
@@ -564,10 +356,6 @@
             this.PanelContenedorSistemaTradicional.ResumeLayout(false);
             this.PanlAkari.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
-            this.panel3.ResumeLayout(false);
-            this.panel4.ResumeLayout(false);
-            this.panel2.ResumeLayout(false);
-            this.panel5.ResumeLayout(false);
             this.panel6.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -582,15 +370,8 @@
         private System.Windows.Forms.Button btn8040_3_Vias;
         private System.Windows.Forms.Button btn6030_2_Vias;
         private System.Windows.Forms.Button btn6030_3_Vias;
-        private System.Windows.Forms.Button btnEuro_2_Vias;
-        private System.Windows.Forms.Button btnEuro_3_Vias;
         private System.Windows.Forms.Button btnBackSistema;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button btnEUFijo;
-        private System.Windows.Forms.Button btnEuPuerta2Vias;
-        private System.Windows.Forms.Button btnEuPuerta3Vias;
-        private System.Windows.Forms.Button btnPuertaLujo;
-        private System.Windows.Forms.Button btnPuertaBaño;
         private System.Windows.Forms.Button btnCedazoAkari;
         private System.Windows.Forms.Label lblVtNormal;
         private System.Windows.Forms.Panel PanelSistemaTradicional;
@@ -598,14 +379,6 @@
         private System.Windows.Forms.Label lblSistemasAkari;
         private System.Windows.Forms.Panel PanelContenedorSistemaTradicional;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label lblSistemasEuropa;
-        private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Panel panel4;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Panel panel5;
-        private System.Windows.Forms.Label lblPuertas;
-        private System.Windows.Forms.Button btnPuertaLiviana;
-        private System.Windows.Forms.Button btnPuertaEuAbatible;
         private System.Windows.Forms.Button btn50203Vias;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button btn;
